7c56e7de65fee50d4eca41f50f6a8509ef18deea
Tim Traver <timtraver@gmail.com>
2026-03-18 10:23:41 -0700

    stuff

--- compositor-helper.sh
+++ compositor-helper.sh
@@ -46,18 +46,18 @@ exec gst-launch-1.0 -v \
   ! multipartmux boundary=--jpgboundary \
   ! tcpserversink host=0.0.0.0 port=8555 sync=false recover-policy=keyframe \
   \
-  v4l2src device=$CAMERA_DEVICE do-timestamp=true \
-  ! image/jpeg,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1 \
-  ! jpegdec \
-  ! videoconvert \
-  ! video/x-raw,format=RGBA,width=$WIDTH,height=$HEIGHT \
+  tcpclientsrc host=127.0.0.1 port=8556 do-timestamp=true \
+  ! video/x-raw,format=RGBA,width=$WIDTH,height=$HEIGHT,framerate=5/1 \
   ! videorate \
   ! video/x-raw,framerate=$FRAMERATE/1 \
   ! queue \
   ! mix.sink_0 \
   \
-  tcpclientsrc host=127.0.0.1 port=8556 do-timestamp=true \
-  ! video/x-raw,format=RGBA,width=$WIDTH,height=$HEIGHT,framerate=5/1 \
+  v4l2src device=$CAMERA_DEVICE do-timestamp=true \
+  ! image/jpeg,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1 \
+  ! jpegdec \
+  ! videoconvert \
+  ! video/x-raw,format=RGBA,width=$WIDTH,height=$HEIGHT \
   ! videorate \
   ! video/x-raw,framerate=$FRAMERATE/1 \
   ! queue \
